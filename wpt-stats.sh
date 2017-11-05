4f8eb6b4d5aa05f8fe74ed867bbdfabbae4b109e
Philip Jägenstedt <philip@foolip.org>
2017-11-05 15:02:04 -0800

    wpt-stats: extract out since/until

--- wpt-stats.sh
+++ wpt-stats.sh
@@ -12,9 +12,11 @@ for month in {2015,2016,2017}-{01,02,03,04,05,06,07,08,09,10,11,12}; do
     nextmonth=`date +%Y-%m -d "$month-01 +1 month"`
     # BSD date
     #nextmonth=`date -j -f %Y-%m-%d -v+1m "$month-01" +%Y-%m`
-    commits=`git rev-list origin/master --no-merges --since $month-01T00:00:00Z --until $nextmonth-01T00:00:00Z --count`
-    chromium_exports=`git rev-list origin/master --no-merges --since $month-01T00:00:00Z --until $nextmonth-01T00:00:00Z --count --grep "^Change-Id:" --grep "^Cr-Commit-Position:"`
-    gecko_exports=`git rev-list origin/master --no-merges --since $month-01T00:00:00Z --until $nextmonth-01T00:00:00Z --count --grep "^Upstreamed from https://bugzilla\\.mozilla\\.org/"`
-    servo_exports=`git rev-list origin/master --no-merges --since $month-01T00:00:00Z --until $nextmonth-01T00:00:00Z --count --grep "^Upstreamed from https://github\\.com/servo/"`
+    since="$month-01T00:00:00Z"
+    until="$nextmonth-01T00:00:00Z"
+    commits=`git rev-list origin/master --no-merges --since="$since" --until="$until" --count`
+    chromium_exports=`git rev-list origin/master --no-merges --since="$since" --until="$until" --count --grep "^Change-Id:" --grep "^Cr-Commit-Position:"`
+    gecko_exports=`git rev-list origin/master --no-merges --since="$since" --until="$until" --count --grep "^Upstreamed from https://bugzilla\\.mozilla\\.org/"`
+    servo_exports=`git rev-list origin/master --no-merges --since="$since" --until="$until" --count --grep "^Upstreamed from https://github\\.com/servo/"`
     echo "$month,$commits,$chromium_exports,$gecko_exports,$servo_exports"
 done
