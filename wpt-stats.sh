e0649103f55d4b09aa4c9349a032d44b83f7d5d5
Philip Jägenstedt <philip@foolip.org>
2017-11-05 12:48:31 -0800

    Write upstream stats as CSV

--- wpt-stats.sh
+++ wpt-stats.sh
@@ -6,6 +6,7 @@ set -o pipefail
 WPT_DIR="$1"
 cd "$WPT_DIR"
 
+echo "Month,Commits,Chromium exports,Gecko exports,Servo exports"
 for month in {2015,2016,2017}-{01,02,03,04,05,06,07,08,09,10,11,12}; do
     # GNU date
     nextmonth=`date +%Y-%m -d "$month-01 +1 month"`
@@ -15,5 +16,5 @@ for month in {2015,2016,2017}-{01,02,03,04,05,06,07,08,09,10,11,12}; do
     chromium_exports=`git rev-list origin/master --no-merges --since $month-01T00:00:00Z --until $nextmonth-01T00:00:00Z --count --grep "^Change-Id:" --grep "^Cr-Commit-Position:"`
     gecko_exports=`git rev-list origin/master --no-merges --since $month-01T00:00:00Z --until $nextmonth-01T00:00:00Z --count --grep "^Upstreamed from https://bugzilla\\.mozilla\\.org/"`
     servo_exports=`git rev-list origin/master --no-merges --since $month-01T00:00:00Z --until $nextmonth-01T00:00:00Z --count --grep "^Upstreamed from https://github\\.com/servo/"`
-    echo -e "$month\t$commits\t$chromium_exports\t$gecko_exports\t$servo_exports"
+    echo "$month,$commits,$chromium_exports,$gecko_exports,$servo_exports"
 done
