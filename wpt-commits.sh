b207081b684e61a2115e659adfa26babc35da251
Philip Jägenstedt <philip@foolip.org>
2018-03-01 12:41:19 -0500

    Also account for the new style of Gecko exports
    https://github.com/w3c/web-platform-tests/pulls?q=is%3Apr+label%3Amozilla%3Agecko-sync+is%3Aclosed+sort%3Acreated-asc

--- wpt-commits.sh
+++ wpt-commits.sh
@@ -16,7 +16,7 @@ for month in {2015,2016,2017,2018}-{01,02,03,04,05,06,07,08,09,10,11,12}; do
     until="$nextmonth-01T00:00:00Z"
     commits=`git rev-list origin/master --no-merges --since="$since" --until="$until" --count`
     chromium_exports=`git rev-list origin/master --no-merges --since="$since" --until="$until" --count --grep "^Change-Id:" --grep "^Cr-Commit-Position:"`
-    gecko_exports=`git rev-list origin/master --no-merges --since="$since" --until="$until" --count --grep "^Upstreamed from https://bugzilla\\.mozilla\\.org/"`
+    gecko_exports=`git rev-list origin/master --no-merges --since="$since" --until="$until" --count --grep "^Upstreamed from https://bugzilla\\.mozilla\\.org/" --grep "^gecko-commit:"`
     servo_exports=`git rev-list origin/master --no-merges --since="$since" --until="$until" --count --grep "^Upstreamed from https://github\\.com/servo/"`
     webkit_exports=`git rev-list origin/master --no-merges --since="$since" --until="$until" --count --grep "^WebKit export of https://bugs\\.webkit\\.org/"`
     echo "$month,$commits,$chromium_exports,$gecko_exports,$servo_exports,$webkit_exports"
