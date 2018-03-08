27bac059af4624938d502d2738a5bf834444328e
Robert Ma <robertma@chromium.org>
2018-03-08 13:48:58 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,13 @@ CHROMIUM_DIR="$HOME/chromium/src"
 WPT_DIR="$HOME/web-platform-tests"
 
 rm -rf out; mkdir out
+# Check out databases from gh-pages, which may fail harmlessly.
+git fetch origin || true
+# Don't combine them into one line in case some file doesn't exist.
+git checkout origin/gh-pages -- wpt-prs.csv || true
+git checkout origin/gh-pages -- import-latencies.csv || true
+git checkout origin/gh-pages -- export-latencies.csv || true
+git reset HEAD *.csv || true
 
 echo "upstream wpt commit stats..."
 ./wpt-commits.sh "$WPT_DIR" > out/wpt-commits.csv
@@ -14,13 +21,13 @@ echo
 
 echo "chromium import stats..."
 python wpt-import-stats.py "$CHROMIUM_DIR" "$WPT_DIR"
-mv import-latencies.csv out/chromium-import-latency.csv
 echo
 
 echo "chromium export stats..."
 python wpt-export-stats.py "$CHROMIUM_DIR"
-mv export-latencies.csv out/chromium-export-latency.csv
 echo
+
+mv *.csv out/
 
 echo "chromium usage stats..."
 cp wpt-usage-stats "$CHROMIUM_DIR/third_party/WebKit/Tools/Scripts/"
